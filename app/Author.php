ce6b720607b9edb72def9a6a15a1e4558f338755
Manson Xasthur <mansonx13@gmail.com>
2019-10-25 15:19:23 +0200

    add path to author resource

---
+++ app/Author.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    protected $guarded = [];
+
+    protected $dates = [
+        'dob',
+    ];
+
+    /**
+     * @param $dob
+     */
+    public function setDobAttribute($dob)
+    {
+        $this->attributes['dob'] = Carbon::parse($dob);
+    }
+}
