c8dbd4e79e4c9b7b4aaa58cc5cba563eddaca270
Manson Xasthur <mansonx13@gmail.com>
2019-10-25 15:20:49 +0200

    add book management test

---
+++ tests/Feature/BookManagementTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Victor',
+        ]);
+
+        $book = Book::first();
+        $this->assertNotNull($book);
+        $response->assertRedirect($book->path());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function an_author_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Name',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Victor',
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch($book->path(), [
+            'title' => 'New Title',
+            'author' => 'New Author',
+        ]);
+
+        $this->assertEquals('New Title', $book->fresh()->title);
+        $this->assertEquals('New Author', $book->fresh()->author);
+
+        $response->assertRedirect($book->path());
+    }
+
+    /** @test */
+    public function a_book_can_be_deleted()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Victor',
+        ]);
+
+        $book = Book::first();
+        $this->assertNotNull($book);
+
+        $response = $this->delete($book->path());
+
+        $this->assertNull(Book::first());
+        $response->assertRedirect('/books');
+    }
+}
